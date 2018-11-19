acade41087ef0a8f5d23d67d6e8ed933bc4b42d6
Rafal Skrzydelski <alfa1986beta@gmail.com>
2018-11-19 21:19:25 +0100

    Skeleton suplement

--- gui.py
+++ gui.py
@@ -12,11 +12,33 @@ class GUI(wx.Frame):
         self.panel = wx.Panel(self)
         self.panel.SetBackgroundColour(wx.WHITE)
 
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.text = wx.TextCtrl(self.panel, size=(250, 25), style=wx.TE_READONLY)
         self.AddClientButton = wx.Button(self.panel, -1, 'Add Client')
         self.AddClientButton.Bind(wx.EVT_BUTTON, self.AddClient)
 
+        hbox1.Add(self.text, proportion=1, flag=wx.ALIGN_CENTER)
+        hbox2.Add(self.AddClientButton, proportion=1, flag = wx.RIGHT, border = 10)
+
+        vbox.Add((0, 30))
+        vbox.Add(hbox1, flag=wx.ALIGN_CENTER)
+        vbox.Add((0, 50))
+        vbox.Add(hbox2, proportion=1, flag=wx.ALIGN_CENTRE)
+
+        self.panel.SetSizer(vbox)
+        self.Centre()
+        self.Show(True)
+
     def AddClient(self, event):
-        print('Add Client')
+        dlg = wx.TextEntryDialog(self, 'Enter Client Name', 'Text Entry Dialog')
+
+        if dlg.ShowModal() == wx.ID_OK:
+            self.text.SetValue(dlg.GetValue())
+        dlg.Destroy()
 
 
 
